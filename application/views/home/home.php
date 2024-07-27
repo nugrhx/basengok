4c98a0dc764323f59eef263150d1222cdd522045
nugrhx <nugrhx@gmail.com>
2024-07-27 22:10:26 +0700

    some update
    ganti nama dinas done, wonderful indonesia done

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -1,7 +1,13 @@
 <div class="container">
 	<div class="px-4 py-5 my-5 text-center">
 
-		<img class="d-block mx-auto mb-4 z-n1" src="<?= base_url('assets/img/logo.png') ?>" alt="" width="54px">
+		<div class="mt-5">
+			<img class="mx-auto mb-4 z-n1" src="<?= base_url('assets/img/logo.png') ?>" alt="" height="72px">
+			&nbsp;
+			<img class="mx-auto mb-4 z-n1" src="<?= base_url('assets/img/wonderful.png') ?>" alt="" height="72px">
+		</div>
+
+		&nbsp;
 
 		<p class="px-4">
 			Selamat Datang di
@@ -12,7 +18,7 @@
 		</h1>
 
 		<div class="col-lg-4 mx-auto" style="font-size: 14px;">
-			<p class="lead mb-4">DINAS KEBUDAYAAN, KEPEMUDAAN, OLAHRAGA DAN PARIWISATA KABUPATEN PULANG PISAU.</p>
+			<p class="lead mb-4">DINAS KEBUDAYAAN, KEPEMUDAAN DAN OLAHRAGA, DAN PARIWISATA KABUPATEN PULANG PISAU.</p>
 			<div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
 				<a href="/dtw" class="btn btn-light btn-md px-4 gap-3 rounded-5">
 					EXPLORE BASENGOK
